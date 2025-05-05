8efaa0ec6926e4a06892beaceed26217d57afe28
milandarai90 <milandarai90@gmail.com>
2025-05-06 00:52:14 +0545

    verify otp ko ni services ra model banauna parxa

--- lib/activity/verify_otp_activity.dart
+++ lib/activity/verify_otp_activity.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class verifyOTP_activity extends StatefulWidget {
 
@@ -9,12 +10,79 @@ class verifyOTP_activity extends StatefulWidget {
 }
 
 class verifyOTP_state extends State<verifyOTP_activity>{
+
+  @override
+  void dispose() {
+    controllers.forEach((c) => c.dispose());
+    focusNode.forEach((f)=> f.dispose());
+    super.dispose();
+  }
+
+  final List<TextEditingController> controllers = List.generate(5, (_) => TextEditingController());
+  final List<FocusNode> focusNode = List.generate(5,(_) => FocusNode());
+
+
+  Widget otpBoxes(int index){
+    return Container(
+      color: Theme.of(context).canvasColor,
+      width: 50,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: TextField(
+        controller: controllers[index],
+        focusNode: focusNode[index],
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        maxLength: 1,
+        decoration: InputDecoration(counterText: ""),
+        onChanged: (value){
+          if(value.length == 1 && index < 4){
+            focusNode[index+1].requestFocus();
+          }else if(value.isEmpty && index > 0){
+            focusNode[index-1].requestFocus();
+          }
+        },
+      ),
+    );
+}
+void verifyOTP(){
+    String otp = controllers.map((c)=>c.text).join();
+    ////////////////////////////////////////////////
+}
+
   @override
   Widget build(BuildContext context) {
 
   return Scaffold(
-    body: Center(
-
+    body: Container(
+      height: MediaQuery.of(context).size.height,
+      color: Theme.of(context).primaryColor,
+      child: Center(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(5, otpBoxes),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                  onPressed: verifyOTP,
+                  child: Text("SUBMIT",style: TextStyle(color: Theme.of(context).primaryColor,fontSize: 16,fontWeight: FontWeight.w500),),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).canvasColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
   );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iapply3/activity/splashscreen_activity.dart';
+import 'package:iapply3/activity/verify_otp_activity.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: splashscreen_activity(),
+      home: verifyOTP_activity(),
     );
   }
 }
